11c3d83e62385dfcabfd389d61dbd8308780e3ba
TTHR <yhr199792@gmail.com>
2025-10-14 23:12:40 +0800

    折线图数据处理

--- market_cost/app/api/market_api.py
+++ market_cost/app/api/market_api.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from services.market_service import get_all_items, build_view_data
 
 api = Flask(__name__)
 
@@ -6,12 +7,13 @@ api = Flask(__name__)
 # 获取所有物品
 @api.route("/api/items", methodes=["GET"])
 def get_all_items():
-    return
+    return get_all_items()
 
 
 # 获取选中物品的价格
 @api.route("/api/items", methodes=["GET"])
 def get_item_cost():
-    names_param = request.args.get("names")
-
-    return
+    names_param = request.args.getlist("names")
+    if not names_param:
+        return []
+    return build_view_data(names_param)
--- market_cost/app/services/market_service.py
+++ market_cost/app/services/market_service.py
@@ -1,5 +1,7 @@
 from utils import file_utils
 from utils.i18n import i18n
+import os, glob, json
+from datetime import datetime
 
 
 # 获取所有商品
@@ -13,5 +15,73 @@ def get_all_items():
 
 
 # 根据物品名构建折线图数据
-def build_view_data():
-    return
+def build_view_data(names: list[str]):
+    files = glob.glob("data/*.json")
+    # 返回的数据
+    records = []
+    for file_name in files:
+        timestamp = int(os.path.splitext(os.path.basename(file_name))[0])
+        # 时间
+        item_time = datetime.fromtimestamp(timestamp).strftime("%m-%d %H时")
+
+        with open(file_name, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        for name in names:
+            # 组装数据
+            isExist = get_value(name, records)
+            # 总数据
+            value = get_value(name, data)
+
+            if isExist:
+                for item in isExist:
+                    # 买入
+                    a_data = item.setdefault("a", {}).setdefault("data", [])
+                    a = value.get("a")
+                    a_data = a_data + a
+
+                    # 卖出
+                    b_data = item.get("b", {}).get("data")
+                    b = value.get("b")
+                    b_data = b_data + b
+                    records.append(
+                        {
+                            "key": name,
+                            "name": "买入",
+                            "type": "line",
+                            "stack": "Total",
+                            "data": a_data,
+                        },
+                        {
+                            "key": name,
+                            "name": "卖出",
+                            "type": "line",
+                            "stack": "Total",
+                            "data": b_data,
+                        },
+                    )
+            else:
+                records.append(
+                    {
+                        "key": name,
+                        "name": "买入",
+                        "type": "line",
+                        "stack": "Total",
+                        "data": value.get("a"),
+                    },
+                    {
+                        "key": name,
+                        "name": "卖出",
+                        "type": "line",
+                        "stack": "Total",
+                        "data": value.get("b"),
+                    },
+                )
+    return records
+
+
+def get_value(k: str, data: json):
+    for item in data:
+        if k in item:
+            value = item[k]
+            return value
